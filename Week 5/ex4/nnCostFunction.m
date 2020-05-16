2439fd25fc98da3ccde8429870d6762703fee48b
Mike <Tatlreach@gmail.com>
2020-05-15 22:29:22 -0500

    my solutions to week5 programming assignment

--- Week 5/ex4/nnCostFunction.m
+++ Week 5/ex4/nnCostFunction.m
@@ -38,7 +38,26 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-%
+a2 = sigmoid([ones(m,1) X] * Theta1');
+
+a3 = sigmoid([ones(m,1) a2] * Theta2');
+% NOTE: check the correct dimensions here
+% y_out = a2 > 0.5;
+% size(a2)
+
+% recode y to be of 1s, 0s
+training_ans = zeros(m, num_labels);
+for i=1:m,
+    training_ans(i, y(i)) = 1;
+end
+y = training_ans;
+
+% cost function for individual values (logistic regression?)
+% J = -(1/m) * (y' .* log(a2)) + ((1-y)' .* log(1-a2)) ));
+
+% cost func for (m by K) matrix of answers
+J = -(1/m) * sum(sum( (y .* log(a3)) + ((1-y) .* log(1-a3)) ));
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -53,7 +72,58 @@ Theta2_grad = zeros(size(Theta2));
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
-%
+
+% init deltasums to 0
+Theta1_grad = zeros(size(Theta1));
+Theta2_grad = zeros(size(Theta2));
+for t=1:m,
+    % get data instance x
+    a1 = [1 X(t,:)];
+    
+    % forward feed x
+    z2 = a1 * Theta1';
+    a2 = [1, sigmoid(z2)];
+    
+    z3 = a2 * Theta2';
+    a3 = sigmoid(z3);
+    % a3 /* output */  doesn't need a bias unit attached
+    
+    % get final delta = (forward_feed_ans - y);
+    yt = y(t,:);
+    delta_a3 = (a3 - yt);
+    
+    % backpropagate delta
+        % g'(z(l)) = z(l) .* (1-z(l));
+        % delta2 = (theta2' * delta3) .* gz_prime
+        % add delta_theta vals to delta sum
+        
+    % their a2 = my [1, a2];
+    % my a2 = their sigmoid(z2) = my sigmoid([1 a1] * Theta1')
+        
+    % NOTE: NO BIAS UNIT  in z2, a3, delta_a3
+    gz_prime = sigmoidGradient( z2 );
+    
+    % NOTE: delta_a3 does NOT have BIAS
+    % NOTE: result(delta_a2) has BIAS because Theta2 does
+    delta_a2 = (delta_a3 * Theta2) .* [1 gz_prime];
+    
+    %remove bias column
+    delta_a2 = delta_a2(2:end);
+    
+    % calc delta_Theta2(delta_a3' * a2)
+    % add to Theta2_sum
+    Theta2_grad = Theta2_grad + (delta_a3' * a2);
+    % NOTE  (K x m) * (m x a2_sz+1) = (K x a2_sz+1) = size(Theta2)
+    
+    % calc delta_Theta1(delta_a2' * a1)
+    % not this?? % Theta1_grad = Theta1_grad + delta1;
+    Theta1_grad = Theta1_grad + (delta_a2' * a1);
+    % NOTE  (a2_sz x m) * (m x a1_sz+1) = (a2_sz x a1_sz+1) = size(Theta2)
+end
+% average delta_theta sums
+Theta1_grad = Theta1_grad/m;
+Theta2_grad = Theta2_grad/m;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -62,8 +132,30 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% remove 1st column
+%Theta1_reg = Theta1(:, 2:size(Theta1,2));
+%Theta2_reg = Theta2(:, 2:size(Theta2,2));
+% NOTE: size(mtrx,2) is col size
 
+% regularize the theta vals
+% set first column to 0
+Theta1_reg = Theta1;
+Theta2_reg = Theta2;
+Theta1_reg(:, 1) = zeros(size(Theta1,1), 1);
+Theta2_reg(:, 1) = zeros(size(Theta2,1), 1);
 
+% add regularized component of delta thetas
+Theta1_grad = Theta1_grad + (lambda/m) * Theta1_reg;
+Theta2_grad = Theta2_grad + (lambda/m) * Theta2_reg;
+
+% unroll delta_theta sums into a vector
+grad = [Theta1_grad(:); Theta2_grad(:)];
+
+% square thetas
+Theta1_reg = Theta1_reg .^ 2;
+Theta2_reg = Theta2_reg .^ 2;
+
+J = J + (lambda/(2*m)) * (sum(sum(Theta1_reg)) + sum(sum(Theta2_reg)));
 
 
 
